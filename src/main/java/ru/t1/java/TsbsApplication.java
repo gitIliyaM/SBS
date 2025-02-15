ddec4510209a5fded31da15da68efe2dbc824560
WiseAdviceIM <wiseadviceim@roadmapschool.ru>
2025-02-15 19:45:52 +0300

    SBS

---
+++ src/main/java/ru/t1/java/TsbsApplication.java
@@ -0,0 +1,13 @@
+package ru.t1.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsbsApplication.class, args);
+    }
+
+}
